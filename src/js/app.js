b26b5429ce2ceadd345201bee40f4fa04b497017
germanguaimare <germanguaimare@gmail.com>
2022-01-24 15:05:46 +0000

    working

--- src/js/app.js
+++ src/js/app.js
@@ -20,6 +20,7 @@ import "../style/index.css";
         role: null,
         country: null,
         city: null
+
     }
  */
 function render(variables = {}) {
@@ -30,17 +31,25 @@ function render(variables = {}) {
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
+  document.querySelector("#widget_content").innerHTML = `(<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>"${variables.name + " " + variables.lastname}"</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i>${
+              variables.twitter
+            }</a></li>
+            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i>${
+              variables.github
+            }</a></li>
+            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i>${
+              variables.linkedin
+            }</a></li>
+            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i>${
+              variables.instagram
+            }</a></li>
           </ul>
         </div>
     `;
